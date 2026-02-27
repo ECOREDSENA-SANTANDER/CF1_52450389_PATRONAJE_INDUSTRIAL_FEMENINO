a51cd74d9f5746f08963865f13c331664fccb280
PABG1990 <pedro19908454@gmail.com>
2026-02-27 15:48:01 -0500

    av1

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,7 +2,7 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. 
+    p Lorem ipsum dolocing elit. Mauris eu tincidunt erat. 
     
 
     .row.justify-content-center
