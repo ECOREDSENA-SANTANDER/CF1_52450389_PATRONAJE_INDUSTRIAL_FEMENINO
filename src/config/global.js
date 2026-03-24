5239c314cae8a7bf2e35bb6c2b9550eb6516b8fe
PABG1990 <pedro19908454@gmail.com>
2026-03-24 13:56:28 -0500

    Add_CFA

--- src/config/global.js
+++ src/config/global.js
@@ -130,7 +130,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CF1_52450389_DU.pdf',
       },
       {
         icono: 'fas fa-download',
