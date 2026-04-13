889e3894d12b74588058a95dfc2a635c6d45f3e3
PABG1990 <pedro19908454@gmail.com>
2026-04-13 10:28:14 -0500

    Act

--- src/config/global.js
+++ src/config/global.js
@@ -130,7 +130,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/CF1_52450389_DU.pdf',
+        download: 'downloads/CF1_52450389__DU.zip',
       },
       {
         icono: 'fas fa-download',
